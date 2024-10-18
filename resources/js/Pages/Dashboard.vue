5d3a4bb456d0c460002127076fb2c6d3c583e3e8
EdSabhin <edsabhin@yahoo.com>
2024-10-17 20:12:55 -0500

    Added Articles Pages folder, with Index.vue for main articles view.

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import { Head } from '@inertiajs/vue3';
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
+import { Head } from "@inertiajs/vue3";
 </script>
 
 <template>
@@ -8,21 +8,22 @@ import { Head } from '@inertiajs/vue3';
 
     <AuthenticatedLayout>
         <template #header>
-            <h2
-                class="text-xl font-semibold leading-tight text-gray-800"
-            >
-                Dashboard
-            </h2>
+            <div class="w-full flex flex-col items-start gap-8 pb-4">
+                <h2 class="text-xl font-semibold leading-tight text-gray-800">
+                    Dashboard
+                </h2>
+                <a
+                    :href="route('articles')"
+                    class="p-3 text-center border border-indigo-400 rounded"
+                    >Articles</a
+                >
+            </div>
         </template>
 
         <div class="py-12">
             <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-                <div
-                    class="overflow-hidden bg-white shadow-sm sm:rounded-lg"
-                >
-                    <div class="p-6 text-gray-900">
-                        You're logged in!
-                    </div>
+                <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
+                    <div class="p-6 text-gray-900">You're logged in!</div>
                 </div>
             </div>
         </div>
